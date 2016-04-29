290e46c283d601560835a2e6537db5f638ff391f
Richard Zhang <zhang.richard2016@gmail.com>
2016-04-28 20:00:40 -0400

    2nd init commit
    2nd init commit

---
+++ Assets/Scripts/Generate_Obstacles.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Generate_Obstacles : MonoBehaviour
+{
+    public GameObject recyclable_1;
+    public GameObject unrecyclable_1;
+    public GameObject recyclable_2;
+    int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacleUnrecyclable", 1f, 2f);
+        InvokeRepeating("CreateObstacleRecyclable", 1f, 2f);
+
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        GUILayout.Label("Current Score: " + score.ToString());
+    }
+
+   
+    void CreateObstacleRecyclable()
+    {
+        Invoke("CreateObstacleRecyclable_1", Random.Range(1,5));
+        Invoke("CreateObstacleRecyclable_2", Random.Range(1, 5));
+    }
+    void CreateObstacleRecyclable_1()
+    {
+        if (recyclable_1 != null)
+        {
+            Instantiate(recyclable_1);
+            score++;
+        }
+    }
+    void CreateObstacleRecyclable_2()
+    {
+        if (recyclable_2 != null)
+        {
+            Instantiate(recyclable_2);
+            score++;
+        }
+    }
+    void CreateObstacleUnrecyclable_1()
+    {
+        if (unrecyclable_1 != null)
+        {
+            Instantiate(unrecyclable_1);
+            score++;
+        }
+    }
+    void CreateObstacleUnrecyclable(){
+        Invoke("CreateObstacleUnrecyclable_1", Random.Range(1, 5));
+    }
+}
---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player_Controller : MonoBehaviour
+{
+
+    public Vector2 jumpForce = new Vector2(0, 300);
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().AddForce(jumpForce);
+        }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Die by collision
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.name == "newspaper")
+        {
+            Destroy(other.gameObject);
+        } else if (other.gameObject.name== "shoe")
+        {
+            SceneManager.LoadScene(0);
+
+        }
+        else if (other.gameObject.name == "sodacan")
+        {
+            Destroy(other.gameObject);
+
+        }
+    }
+
+}
